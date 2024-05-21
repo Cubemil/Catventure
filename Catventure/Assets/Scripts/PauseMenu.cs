951551ebbef484aa948b5a512b7a0388438fd6d6
Cardostyle <cardostyle19@gmail.com>
2024-05-21 21:00:46 +0200

    Main Menu Scene erstellt und Pause Menü mit den Funktionen Quit, Resume und Main Menu erstellt (in extra scene PauseMenu)

---
+++ Catventure/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused;
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        GameIsPaused = true;
+        pauseMenuUi.SetActive(GameIsPaused);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        GameIsPaused = false;
+        pauseMenuUi.SetActive(GameIsPaused);
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting...");
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        Debug.Log("Main Menu loading...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scenes/MainMenuScene");
+    }
+    
+
+}
