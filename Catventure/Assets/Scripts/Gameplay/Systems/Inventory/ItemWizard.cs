ecc6b3324384999baf0643ec2041b5cb743bbb9b
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-09-03 09:17:52 +0200

    renamed files and refactoring

---
+++ Catventure/Assets/Scripts/Gameplay/Systems/Inventory/ItemWizard.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Gameplay.Systems.Inventory
+{
+    public class ItemWizard : ScriptableWizard
+    {
+        public string itemName;
+        public string description;
+        public Sprite icon;
+        public int stackSize;
+        public int id;
+
+        [MenuItem("Assets/Items/Item")]
+        public static void Create()
+        {
+            ScriptableWizard.CreateWindow<ItemWizard>();
+        }
+
+        private void OnWizardCreate()
+        {
+            var i = ScriptableObject.CreateInstance<ItemCreate>();
+            
+            i.itemName = itemName;
+            i.description = description;
+            i.icon = icon;
+            i.stackSize = stackSize;
+            i.id = id;
+        
+            AssetDatabase.CreateAsset(i, "Assets/Items/" + itemName + "_item" + ".asset");
+            AssetDatabase.Refresh();
+        }
+    }
+}
