544ec1d1018505f735e97eb5c9cb4a75c63ed557
Felix Gahler <felixbenjamin.gahler@student.hs-anhalt.de>
2024-09-09 17:35:18 +0200

    Finalised apartment scene

--- Catventure/Assets/Scripts/Gameplay/Interaction/CatNipInteractable.cs
+++ Catventure/Assets/Scripts/Gameplay/Interaction/CatNipInteractable.cs
@@ -1,13 +1,30 @@
-﻿using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace Gameplay.Interaction
 {
     public class CatNipInteractable : ItemInteractable
     {
+        public GameObject cat;
+        private Animator _catAnimator;
+        private static readonly int IsSleeping = Animator.StringToHash("isSleeping");
+
+        void Start()
+        {
+            _catAnimator = cat.GetComponent<Animator>();
+        }
+
         public override void Interact()
         {
-            if (gameObject) gameObject.SetActive(false);
-            SceneManager.LoadScene("Apartment");
+            _catAnimator.SetBool(IsSleeping, true);
+            StartCoroutine(DelayedSceneChange(5f)); // Start the coroutine with a 5-second delay
+        }
+        
+        private IEnumerator DelayedSceneChange(float delay)
+        {
+            yield return new WaitForSeconds(delay); // Wait for the specified delay
+            SceneManager.LoadScene("Garden"); // Change the scene after the delay
         }
     }
 }
